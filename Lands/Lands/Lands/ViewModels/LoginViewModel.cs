763a195a1211a648fcf826a8edf38f5b76968452
Andy Rosete <andyrosete17@gmail.com>
2018-10-24 01:26:47 +0200

    Login viewModel with Base viewModel

--- Lands/Lands/Lands/ViewModels/LoginViewModel.cs
+++ Lands/Lands/Lands/ViewModels/LoginViewModel.cs
@@ -2,20 +2,40 @@
 namespace Lands.ViewModels
 {
     using GalaSoft.MvvmLight.Command;
-    using System;
+    using System.ComponentModel;
     using System.Windows.Input;
+    using Xamarin.Forms;
 
-    public class LoginViewModel
+    public class LoginViewModel : BaseViewModel
     {
         #region Properties
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public bool IsRunning { get; set; }
+        public string Email {get;set;}
         public bool IsRemembered { get; set; }
+        public string Password
+        {
+            get { return this.password; }
+            set { SetValue(ref this.password, value); }
+        }
+        public bool IsRunning
+        {
+            get { return this.isRunning; }
+            set { SetValue(ref this.isRunning, value); }
+        }
+        public bool IsEnable
+        {
+            get { return this.isEnable; }
+            set { SetValue(ref this.isEnable, value); }
+        }
+        #endregion
+
+        #region Attributes
+        private string password;
+        private bool isRunning;
+        private bool isEnable;
         #endregion
 
         #region Commands
-        public ICommand LoginCommand
+        public ICommand LoginCommand 
         {
             get
             {
@@ -29,12 +49,39 @@ namespace Lands.ViewModels
         public LoginViewModel()
         {
             this.IsRemembered = true;
+            this.IsEnable = true;
         }
+
         #endregion
 
-        private void Login()
+        private async void Login()
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You mus enter an email",
+                    "Accept"
+                    );
+                this.IsRunning = true;
+                this.IsEnable = false;
+                return;
+            }
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter a password",
+                    "Accept"
+                    );
+                this.IsRunning = true;
+                this.IsEnable = false;
+                return;
+            }
+
+            this.IsRunning = false;
+            this.IsEnable = true;
+
         }
     }
 }
