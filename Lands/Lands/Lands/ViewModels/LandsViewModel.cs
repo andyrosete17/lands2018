e637dd4492d586ab54655e0dee84c6ec0a359224
Andy Rosete <andyrosete17@gmail.com>
2018-10-29 00:18:54 +0100

    Implemented getlist service to get lands, updated model

--- Lands/Lands/Lands/ViewModels/LandsViewModel.cs
+++ Lands/Lands/Lands/ViewModels/LandsViewModel.cs
@@ -1,6 +1,59 @@
-﻿namespace Lands.ViewModels
+﻿
+namespace Lands.ViewModels
 {
-    public class LandsViewModel
+    using Services;
+    using Models;
+    using System;
+    using System.Collections.ObjectModel;
+    using Xamarin.Forms;
+    using System.Collections.Generic;
+
+    public class LandsViewModel : BaseViewModel
     {
+        #region Services
+        private ApiService apiService;
+        #endregion
+
+        #region Attributes
+        private ObservableCollection<Land> lands;
+        #endregion
+
+        #region Properties
+        public ObservableCollection<Land> Lands
+        {
+            get { return this.Lands;}
+            set { SetValue(ref this.lands, value);}
+        }
+        #endregion
+
+        #region Constructors
+        public LandsViewModel()
+        {
+            this.apiService = new ApiService();
+            this.LoadLands();
+        }
+        #endregion
+
+        #region Methods
+        private async void LoadLands()
+        {
+            var response = await this.apiService.GetList<Land>(
+                "http://restcountries.eu",
+                "/rest",
+                "/v2/all");
+
+            if (!response.IsSuccess)
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    response.Message,
+                    "Accept");
+            }
+
+            var list = (List<Land>)response.Result;
+            this.Lands = new ObservableCollection<Land>(list);
+        }
+        #endregion
+
     }
 }
