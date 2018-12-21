47e3869cdcc97a0c1de8e2a5ca81d0dd36d95a44
Andy Rosete <andy.rosete@innovation.group>
2018-12-21 13:07:16 +0100

    Lands detail

--- Lands/Lands/Lands/ViewModels/LandViewModel.cs
+++ Lands/Lands/Lands/ViewModels/LandViewModel.cs
@@ -1,6 +1,26 @@
 ﻿namespace Lands.ViewModels
 {
+    using Lands.Models;
+
     public class LandViewModel
     {
+        #region Properties
+
+        public Land Land
+        {
+            get;
+            set;
+        }
+
+        #endregion Properties
+
+        #region Constructor
+
+        public LandViewModel(Land land)
+        {
+            this.Land = land;
+        }
+
+        #endregion Constructor
     }
 }
