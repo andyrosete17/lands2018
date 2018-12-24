4f8092ab5624b0bad45c49813418818cd58a5316
Andy Rosete <andy.rosete@innovation.group>
2018-12-24 10:32:24 +0100

    Added Border tap develop to show name of the countries instead code.

--- Lands/Lands/Lands/ViewModels/LandViewModel.cs
+++ Lands/Lands/Lands/ViewModels/LandViewModel.cs
@@ -1,15 +1,29 @@
 ﻿namespace Lands.ViewModels
 {
     using Lands.Models;
+    using System.Collections.ObjectModel;
+    using System.Linq;
 
-    public class LandViewModel
+    public class LandViewModel : BaseViewModel
     {
+        #region Attributes
+
+        private ObservableCollection<Border> borders;
+
+        #endregion Attributes
+
         #region Properties
 
         public Land Land
         {
             get;
             set;
+        }
+
+        public ObservableCollection<Border> Borders
+        {
+            get => this.borders;
+            set => SetValue(ref this.borders, value);
         }
 
         #endregion Properties
@@ -19,8 +33,39 @@
         public LandViewModel(Land land)
         {
             this.Land = land;
+            this.LoadBorders();
         }
 
         #endregion Constructor
+
+        #region Methods
+
+        private void LoadBorders()
+        {
+            this.Borders = new ObservableCollection<Border>();
+            foreach (var border in this.Land.Borders)
+            {
+                var land = MainViewModel.GetInstance()
+                                .LandsList
+                                .Where(x => x.Alpha3Code == border)
+                                .FirstOrDefault();
+                if (land != null)
+                {
+                    this.Borders.Add(
+                    new Border
+                    {
+                        Code = land.Alpha3Code,
+                        Name = land.Name
+                    });
+                }
+            }
+
+            if (!this.Borders.Any())
+            {
+                this.Borders.Add(new Border { Name = "This Country has no border" });
+            }
+        }
+
+        #endregion Methods
     }
 }
