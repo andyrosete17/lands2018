2e47dbb7f139ae6f52b85d49ab330bdea6cfd407
Andy Rosete <andy.rosete@innovation.group>
2019-03-11 13:44:21 +0100

    Added icons, menu items.

--- Lands/Lands/Lands/Helpers/Languages.cs
+++ Lands/Lands/Lands/Helpers/Languages.cs
@@ -142,5 +142,20 @@
         {
             get { return Resource.Menu; }
         }
+
+        public static string MyProfile
+        {
+            get { return Resource.MyProfile; }
+        }
+
+        public static string Statistic
+        {
+            get { return Resource.Statistic; }
+        }
+
+        public static string LogOut
+        {
+            get { return Resource.LogOut; }
+        }
     }
 }
--- Lands/Lands/Lands/ViewModels/MenuItemViewModel.cs
+++ Lands/Lands/Lands/ViewModels/MenuItemViewModel.cs
@@ -2,5 +2,9 @@
 {
     public class MenuItemViewModel
     {
+        public string Icon { get; set; }
+        public string Title { get; set; }
+        public string  PageName { get; set; }
+
     }
 }
