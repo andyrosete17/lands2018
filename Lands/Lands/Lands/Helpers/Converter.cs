ef2f607305038b610d4af0280b47cedc9b48f254
Andy Rosete <andy.rosete@innovation.group>
2019-06-24 09:19:17 +0200

    Added sql lite, doesnt work yet

---
+++ Lands/Lands/Lands/Helpers/Converter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Lands.Domain;
+using Lands.Models;
+
+namespace Lands.Helpers
+{
+    public static class Converter
+    {
+        public static UserLocal ToUserLocal(User user)
+        {
+            return new UserLocal
+            {
+                Email = user.Email,
+                FirstName = user.FirstName,
+                ImagePath = user.ImagePath,
+                LastName = user.LastName,
+                Telephone = user.Telephone,
+                UserId = user.UserId,
+                UserTypeId = user.UserTypeId
+            };
+        }
+    }
+}
